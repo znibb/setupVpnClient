a585be1491f8e5bb804b6d2d2488d7d4f3078043
znibb <pontus.pson@gmail.com>
2016-10-04 14:27:40 +0200

    Updated script to read credentials from file

--- create_VPN_client.sh
+++ create_VPN_client.sh
@@ -1,5 +1,8 @@
 #!/bin/ash
 
+# Source credentials file
+. .credentials
+
 # Check for correct invocation
 if [ -z "$1" -o -z "$2"]; then
   echo "Must specify 2 args"
@@ -7,25 +10,25 @@ if [ -z "$1" -o -z "$2"]; then
 else
   CLIENT=$1
   CLIENT_MAIL=$2
-  SERVER=${SERVER_NAME}		# FIX
+  SERVER_NAME
 
   # Setup dirs
   cd /etc/easy-rsa
   mkdir -p keys/${CLIENT}
 
   # Build and move keys to target folder
-  build-key-pkcs12 ${CLIENT}.at.${SERVER}
-  mv keys/${CLIENT}.at.${SERVER}.* keys/${CLIENT}
+  build-key-pkcs12 ${CLIENT}.at.${SERVER_NAME}
+  mv keys/${CLIENT}.at.${SERVER_NAME}.* keys/${CLIENT}
 
   # Change to target folder and, skeleton conf file and append user specific setting
   cd /etc/easy-rsa/keys/${CLIENT}
-  cp /etc/openvpn/${SERVER}_client.conf ${CLIENT}.at.${SERVER}.conf
-  echo "pkcs12 ${CLIENT}.at.${SERVER}.p12" >> ${CLIENT}.at.${SERVER}.conf
+  cp /etc/openvpn/${SERVER_NAME}_client.conf ${CLIENT}.at.${SERVER_NAME}.conf
+  echo "pkcs12 ${CLIENT}.at.${SERVER_NAME}.p12" >> ${CLIENT}.at.${SERVER_NAME}.conf
 
   # Append TLS key to user conf
-  echo "\n<tls-auth>" >> ${CLIENT}.at.${SERVER}.conf
-  cat ../ta.key >> ${CLIENT}.at.${SERVER}.conf
-  echo "</tls-auth>" >> ${CLIENT}.at.${SERVER}.conf
+  echo "\n<tls-auth>" >> ${CLIENT}.at.${SERVER_NAME}.conf
+  cat ../ta.key >> ${CLIENT}.at.${SERVER_NAME}.conf
+  echo "</tls-auth>" >> ${CLIENT}.at.${SERVER_NAME}.conf
 fi
 
 # Compress and encrypt user dir
@@ -37,8 +40,8 @@ if [ -n "${CLIENT_MAIL}" ]; then
   echo -e "Hello ${CLIENT}!\n\nExtract attached files to your openvpn folder." > mail_body.txt
   echo "Remember to rename *.conf to *.ovpn if you are using Windows." >> mail_body.txt
   echo "This email is automatically generated, please do not respond to it." >> mail_body.txt
-  mailsend -to ${CLIENT_MAIL} -from ${SENDER_EMAIL} -ssl -port 465 -auth-login -smtp ${SENDER_SMTP} -sub "Configuration for ${CLIENT} on ${SERVER} OpenVPN server" -attach "mail_body.txt,text/plain,i" -attach "${CLIENT}.zip" +cc +bc -user ${SENDER_USER} -pass ${SENDER_PASSWORD}
-  
+  mailsend -to ${CLIENT_MAIL} -from ${SENDER_MAIL} -ssl -port 465 -auth-login -smtp ${SENDER_SMTP} -sub "Configuration for ${CLIENT} on ${SERVER_NAME} OpenVPN server" -attach "mail_body.txt,text/plain,i" -attach "${CLIENT}.zip" +cc +bc -user ${SENDER_USER} -pass ${SENDER_PASS}
+
   # Remove tmp mail body file and feedback that mail was sent
   rm mail_body.txt
   echo "Files emailed to ${CLIENT_MAIL}"
