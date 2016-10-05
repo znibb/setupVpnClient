e950c7cf2bbc4f8fe482916865188fdb727a4cf3
znibb <pontus.pson@gmail.com>
2016-10-05 11:28:12 +0200

    Added SERVER_ADDRESS and SERVER_PORT

--- setup.sh
+++ setup.sh
@@ -6,6 +6,10 @@
 echo "Setting up credential file"
 echo -n "Enter VPN server name: "
 read SERVER_NAME
+echo -n "Enter $SERVER_NAME address: "
+read SERVER_ADDRESS
+echo -n "Enter $SERVER_ADDRESS port: "
+read SERVER_PORT
 echo -n "Enter email adress that will be used to send credentials: "
 read SENDER_MAIL
 echo -n "Enter SMTP adress: "
@@ -19,9 +23,11 @@ read -s -r SENDER_PASS
 rm -f .credentials
 touch .credentials
 chmod 600 .credentials
-echo "SERVER_NAME=$SERVER_NAME" >> .credentials
-echo "SENDER_MAIL=$SENDER_MAIL" >> .credentials
-echo "SENDER_SMTP=$SENDER_SMTP" >> .credentials
-echo "SENDER_USER=$SENDER_USER" >> .credentials
-echo "SENDER_PASS=$SENDER_PASS" >> .credentials
+echo "SERVER_NAME=$SERVER_NAME"       >> .credentials
+echo "SERVER_ADDRESS=$SERVER_ADDRESS" >> .credentials
+echo "SERVER_PORT=$SERVER_PORT"       >> .credentials
+echo "SENDER_MAIL=$SENDER_MAIL"       >> .credentials
+echo "SENDER_SMTP=$SENDER_SMTP"       >> .credentials
+echo "SENDER_USER=$SENDER_USER"       >> .credentials
+echo "SENDER_PASS=$SENDER_PASS"       >> .credentials
 echo
